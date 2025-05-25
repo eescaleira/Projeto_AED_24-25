52bd9006d98decfbd2c1884b7a075f889a0be9a8
escaleira <129979364+eescaleira@users.noreply.github.com>
2025-05-25 17:01:19 +0100

    code cleaning

--- main.c
+++ main.c
@@ -6,13 +6,6 @@ int main(){
 		printf("Failed to create item list\n");
 		return -1;
 	}
-    t_item *temp = item_list;
     print_menu(item_list);
-    printf("\n\n");
-    temp = orderByIdItem (item_list);
-    while (temp) {
-        print_item(temp);
-        temp = temp->next;
-    }
 	return 0;
 }
